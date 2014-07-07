697f80b170177079b7a724579641b9d1d3a6f986
Stefano Candori <kendrix16@yahoo.it>
2014-07-07 08:23:03 +0200

    Initial commit.

---
+++ TestCostura/Program.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCostura
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Sample>().As<ISample>();
+            var container = builder.Build();
+        }
+    }
+}
---
+++ TestCostura/Sample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCostura
+{
+    class Sample : ISample
+    {
+        public void Method()
+        {
+            Console.WriteLine("I'm the method");
+        }
+    }
+}
